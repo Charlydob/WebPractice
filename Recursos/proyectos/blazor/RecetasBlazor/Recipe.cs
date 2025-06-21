d0e3a4c84b9011abffc9af2f020117b3de89bd8a
Charlydob <carloseduardooliveirabarbosa@gmail.com>
2025-06-21 10:02:33 +0200

    web blazor de recetas añadida

---
+++ Recursos/proyectos/blazor/RecetasBlazor/Recipe.cs
@@ -0,0 +1,7 @@
+public class Recipe
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Ingredients { get; set; }
+}
